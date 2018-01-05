3e7a93713a113608afddd82ab39d340c5d6bf947
konradjedralski <kjedralski@gmail.com>
2018-01-05 11:46:59 +0100

    initial commit

---
+++ src/main/java/pl/jedralski/LibraryRecommendationSystem/LibraryRecommendationSystemApplication.java
@@ -0,0 +1,12 @@
+package pl.jedralski.LibraryRecommendationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryRecommendationSystemApplication.class, args);
+	}
+}
